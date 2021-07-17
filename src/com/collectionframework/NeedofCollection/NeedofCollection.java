5e0c91e538c03e03f242d0a5f3a32abbf91769fe
abdulraheem@octaware.com <abdulraheem@octaware.com>
2021-07-17 12:21:32 +0530

    01.Java Collections Framework in Depth - 1 - Need for Collections Framework

---
+++ src/com/collectionframework/NeedofCollection/NeedofCollection.java
@@ -0,0 +1,21 @@
+package com.collectionframework.NeedofCollection;
+
+public class NeedofCollection {
+	
+	/*limitation..
+	 * limit in size
+	 * can hold only homogeneous data type 
+	 * array not have API and other library (ready made support no available)
+	 * 
+	 * */
+	public static void main(String[] args) {
+int[] arr=new int [10];
+arr[0]=10;
+arr[2]=20;
+System.out.println(arr[0]);
+System.out.println(arr[2]);
+// collection come in 1.2 version
+//collection is globally in nature
+
+}
+}
